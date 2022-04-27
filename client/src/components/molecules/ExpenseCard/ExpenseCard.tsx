f117b6508b1f1ccfecc219ffbb1428c83e78ca44
Arkadiusz Wydra <hello@awydra.pl>
2022-04-27 23:36:32 +0200

    Created ExpenseCard component

---
+++ client/src/components/molecules/ExpenseCard/ExpenseCard.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react';
+import { Typography, Paper } from '@mui/material';
+
+type Props = {
+  label: string;
+  color: string;
+  amount: string;
+};
+
+const ExpenseCard = ({ label, color, amount }: Props): ReactElement => {
+  return (
+    <Paper
+      sx={{
+        bgcolor: 'background.paper',
+        borderRadius: 2,
+        p: 2,
+        mb: 1,
+      }}
+      elevation={2}
+    >
+      <Typography sx={{ color: 'text.secondary' }}>{label}</Typography>
+      <Typography sx={{ color, fontSize: 16, fontWeight: 'medium' }}>{amount} PLN</Typography>
+    </Paper>
+  );
+};
+
+export default ExpenseCard;
