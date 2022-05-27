af9a034257ffad8ce6befc9a9046ce61eaed08af
Arkadiusz Wydra <hello@awydra.pl>
2022-05-27 15:34:47 +0200

    Refactored routes

--- client/src/routes/MainRoutes.tsx
+++ client/src/routes/MainRoutes.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from 'views/Home/Home';
 import AddReceipt from 'views/AddReceipt/AddReceipt';
 import NotFound from 'views/NotFound/NotFound';
@@ -8,8 +8,9 @@ import { RouteEnum } from 'enums';
 const MainRoutes = (): ReactElement => (
   <Routes>
     <Route path={RouteEnum.HOME} element={<Home />} />
-    <Route path={RouteEnum.ADD_RECEIPT} element={<AddReceipt />} />
-    <Route path="*" element={<NotFound />} />
+    <Route path={RouteEnum.ADD_RECEIPT_STEP} element={<AddReceipt />} />
+    <Route path={RouteEnum.NOT_FOUND} element={<NotFound />} />
+    <Route path="*" element={<Navigate to={RouteEnum.NOT_FOUND} replace />} />
   </Routes>
 );
 
