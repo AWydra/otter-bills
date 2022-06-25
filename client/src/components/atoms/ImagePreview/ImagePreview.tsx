3562715179a9fe45f41d4aaedfd9167a8f5d3e18
Arkadiusz Wydra <hello@awydra.pl>
2022-06-25 15:01:56 +0200

    Added image upload input

---
+++ client/src/components/atoms/ImagePreview/ImagePreview.tsx
@@ -0,0 +1,46 @@
+import { Box } from '@mui/material';
+import React, { ReactElement } from 'react';
+import { ImageType } from 'react-images-uploading';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+
+interface Props {
+  image: ImageType | undefined;
+  onImageUpload: () => void;
+  onImageUpdate: () => void;
+}
+
+const ImagePreview = ({ image, onImageUpload, onImageUpdate }: Props): ReactElement => {
+  return (
+    <Box
+      onClick={image ? onImageUpdate : onImageUpload}
+      sx={{
+        width: (theme) => theme.spacing(7),
+        height: (theme) => theme.spacing(7),
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        border: 'solid 3px',
+        borderColor: (theme) => theme.palette.primary.main,
+        borderRadius: (theme) => theme.spacing(1),
+        overflow: 'hidden',
+        backgroundImage: `url("${image?.dataURL}")`,
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+      }}
+    >
+      {!image && (
+        <AddAPhotoIcon
+          sx={{
+            width: (theme) => theme.spacing(4),
+            height: 'auto',
+            marginLeft: (theme) => theme.spacing(-0.25),
+          }}
+          color="primary"
+        />
+      )}
+    </Box>
+  );
+};
+
+export default ImagePreview;
