2a721dd224d970f2bc52f130a92c47b9ae64364c
Arkadiusz Wydra <hello@awydra.pl>
2023-10-09 01:10:20 +0200

    feature: connected mongodb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION as string)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
 });
