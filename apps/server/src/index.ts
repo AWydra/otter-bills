d44f2fdae6ca15c141366ab7391922d0e05283a6
Arkadiusz Wydra <hello@awydra.pl>
2024-06-23 23:29:33 +0200

    Removed mongoose

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -16,23 +15,10 @@ app.use(
   cors({ origin: ['http://localhost:3000', 'http://192.168.100.12:3000'], credentials: true }),
 );
 
-if (!process.env.MONGODB_CONNECTION) {
-  throw new Error('MongoDB connection string missing');
-}
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-mongoose
-  .connect(process.env.MONGODB_CONNECTION, {
-    dbName: 'otterbills',
-  })
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-app.use('/api', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use(requireAuth);
--- packages/types/src/api.ts
+++ packages/types/src/api.ts
@@ -1,5 +1,3 @@
-import { Types } from 'mongoose';
-
 export interface IAxiosErrorData<T = string> {
   field?: T;
   message: string;
